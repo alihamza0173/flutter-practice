5f5373cbf07d6365fc658b8bff1eaf18579697f9
Ali Hamza <contactalihamza@gmail.com>
2023-10-15 13:02:11 +0500

    staggerdGridView counter

--- flutter_staggered_grid_view/lib/main.dart
+++ flutter_staggered_grid_view/lib/main.dart
@@ -16,7 +16,18 @@ class MainApp extends StatelessWidget {
         body: Center(
           child: StaggeredGridView.countBuilder(
             crossAxisCount: 3,
-            itemBuilder: (context, index) => const Text('data'),
+            itemBuilder: (context, index) => Container(
+              color: index.isEven ? Colors.green : Colors.blue,
+              height: index.isEven ? 200 : 100,
+              padding: const EdgeInsets.all(8),
+              margin: const EdgeInsets.all(8),
+              child: Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text('$index'),
+                ),
+              ),
+            ),
             staggeredTileBuilder: (index) => const StaggeredTile.fit(1),
           ),
         ),
